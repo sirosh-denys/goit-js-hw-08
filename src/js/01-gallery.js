0f364445114e411b9caf34cc6ab67f6d680f3f7e
Denis <sirosh.denys@gmail.com>
2023-04-01 12:42:47 +0300

    Commit1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
 // Add imports above this line
+// Описан в документации
+import SimpleLightbox from 'simplelightbox';
+// Дополнительный импорт стилей
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const refs = {
+  container: document.querySelector(`.gallery`),
+};
+
+const imgMarkup = createImgMarkup(galleryItems);
+
+refs.container.insertAdjacentHTML(`beforeend`, imgMarkup);
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: `alt`,
+  captionDelay: 250,
+});
+
+function createImgMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+        <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>`;
+    })
+    .join(``);
+}
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+// const throttle = require('lodash.throttle');
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const timeOfPause = localStorage.getItem('videoplayer-current-time');
+const parsedTimeOfPause = JSON.parse(timeOfPause);
+
+if (parsedTimeOfPause) {
+  player.setCurrentTime(parsedTimeOfPause);
+}
+
+player.on('timeupdate', throttle(handleTimeUpdate, 1000));
+
+function handleTimeUpdate(event) {
+  //   console.log(event.seconds);
+
+  localStorage.setItem(
+    'videoplayer-current-time',
+    JSON.stringify(event.seconds)
+  );
+}
